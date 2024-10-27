5c4aee26eca01595b6399042652ad45e41d7e2e8
B4-Lord <adrianinholeao2007@gmail.com>
2024-10-26 23:17:16 -0300

    ATUALIZAÇÃO DO JAVASCRIPT + AINDA TEM BUGS

--- script.js
+++ script.js
@@ -19,14 +19,110 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+  function toggleLibraryText() {
+    const imagem = document.querySelector('.imagemarrow');
+    const div = document.getElementById('box-left');
 
-// const hoverDiv = document.getElementById('color');
-// const nav = document.getElementById('box-right');
+    if (imagem.getAttribute('src') === 'right-arrow (1).png') {
+      imagem.setAttribute('src', 'arrow.png'); 
+      div.classList.toggle('opened', true);
+    } else {
+      imagem.setAttribute('src', 'right-arrow (1).png'); 
+      div.classList.toggle('opened', false);
+ 
+    }
+  }
 
-// hoverDiv.addEventListener('mouseenter', function() {
-//     nav.style.background = 'red';
-// });
+// ESSA PARTE A CIMA ESTA CORRETA
 
-// hoverDiv.addEventListener('mouseleave', function() {
-//     nav.style.backgroundColor = 'blue';  
-// });
+let isReduced = false;
+
+function modlplaylisistbar() {
+    const libraryText = document.getElementById("library-text");
+    const boxLeft = document.getElementById("box-left");
+    const libraryImage = document.getElementById("library-image");
+    const removeelement = document.querySelectorAll(".playlistsearch2");
+    const removeelement2 = document.querySelectorAll(".search");
+    const removeelement3 = document.querySelectorAll(".liberus2");
+    const removeelement4 = document.querySelectorAll(".simbolslibrary");
+    const activeheight = document.querySelector(".sexosoes");
+    const sexosoesactivated = document.querySelector(".sexosoes");
+    const centerimg = document.querySelector(".alldivlibrary");
+
+    if (!isReduced) {
+        libraryText.innerText = ""; 
+        boxLeft.classList.add("reducedleft"); 
+        libraryImage.src = "folder (2).png"; 
+
+        const itenstoadd = [
+            ...removeelement, ...removeelement2, ...removeelement3, ...removeelement4
+            
+        ]
+            
+        for (const item of itenstoadd) {
+            item.style.display = "none";
+        }
+
+        // for (const item of removeelement) {
+        //     item.style.display = "none";
+        // }
+
+        // for (const item2 of removeelement2) {
+        //     item2.style.display = "none";
+        // }
+        
+        // for (const item3 of removeelement3) {
+        //     item3.style.display = "none";
+        // }
+
+        // for (const item4 of removeelement4) {
+        //     item4.style.display = "none";
+        // }
+
+        activeheight.classList.add("activeheight");
+
+        sexosoesactivated.classList.add("activeted");
+
+        centerimg.classList.add("centerimg");
+
+        isReduced = true; 
+
+    } else {
+        libraryText.innerText = "Your Library"; 
+        boxLeft.classList.remove("reducedleft"); 
+        libraryImage.src = "folder (1).png"; 
+
+        // for (const item of removeelement) {
+        //     item.style.display = "flex";
+        // }
+
+        // for (const lixo of removeelement2) {
+        //     lixo.style.display = "flex";
+        // }
+
+        // for (const item3 of removeelement3) {
+        //     item3.style.display = "flex";
+        // }
+
+        // for (const item4 of removeelement4) {
+        //     item4.style.display = "flex";
+        // }
+
+        const itenstoremove = [
+            ...removeelement, ...removeelement2, ...removeelement3, ...removeelement4
+            
+        ]
+            
+        for (const item of itenstoremove) {
+            item.style.display = "flex";
+        }
+
+        activeheight.classList.remove("activeheight");
+
+        sexosoesactivated.classList.remove("activeted");
+
+        centerimg.classList.remove("centerimg");
+
+        isReduced = false; 
+    }
+}
